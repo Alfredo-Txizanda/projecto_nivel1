63c429d714bc8ed9f059040f135b8ada4e25d597
Alfredo-Txizanda <txizanda.treinamento@gmail.com>
2025-11-05 16:23:55 +0100

    Modal-Reserva

--- app/view/reserva/Main.php
+++ app/view/reserva/Main.php
@@ -19,18 +19,6 @@
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
                                 <div class="modal-body">
-                                    <!--
-                                    <form>
-                                        <div class="mb-3">
-                                            <label for="recipient-name" class="col-form-label">Recipient:</label>
-                                            <input type="text" class="form-control" id="recipient-name">
-                                        </div>
-                                        <div class="mb-3">
-                                            <label for="message-text" class="col-form-label">Message:</label>
-                                            <textarea class="form-control" id="message-text"></textarea>
-                                        </div>
-                                    </form>
-                                    -->
 
                                     <div class="row mb-3">
                                         <div class="col-md-6 mb-3">
@@ -67,8 +55,8 @@
                                 </div>
 
                                 <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-                                    <button type="submit" class="btn btn-primary">Efectuar Pagamento</button>
+                                    <!--<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>-->
+                                    <button type="submit" class="btn btn-primary w-100">Efectuar Pagamento</button>
                                 </div>
 
                                 <!--

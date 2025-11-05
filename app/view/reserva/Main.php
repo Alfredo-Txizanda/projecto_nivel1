9cd20cfe7755758c0179d6a6ef06414ad16b0c92
Alfredo-Txizanda <txizanda.treinamento@gmail.com>
2025-11-05 15:48:16 +0100

    Reserva-Modal

--- app/view/reserva/Main.php
+++ app/view/reserva/Main.php
@@ -15,10 +15,11 @@
                         <div class="modal-dialog">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h1 class="modal-title fs-5" id="exampleModalLabel">New message</h1>
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Efectuar Reserva</h1>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
                                 <div class="modal-body">
+                                    <!--
                                     <form>
                                         <div class="mb-3">
                                             <label for="recipient-name" class="col-form-label">Recipient:</label>
@@ -29,6 +30,43 @@
                                             <textarea class="form-control" id="message-text"></textarea>
                                         </div>
                                     </form>
+                                    -->
+
+                                    <div class="row mb-3">
+                                        <div class="col-md-6 mb-3">
+                                            <label for="entrada" class="form-label">Entrada</label>
+                                            <input type="date" class="form-control" id="checkIn" required>
+                                        </div>
+                                        <div class="col-md-6 mb-3">
+                                            <label for="saida" class="form-label">Saída</label>
+                                            <input type="date" class="form-control" id="checkOut" required>
+                                        </div>
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label for="room" class="form-label">Quartos</label>
+                                        <select class="form-select" id="room" required>
+                                            <option value="">Seleciona o quarto</option>
+                                            <option value="individual">Quarto Individual</option>
+                                            <option value="duplo">Quarto Duplo</option>
+                                            <option value="twin">Quarto Twin</option>
+                                            <option value="triplo">Quarto Triplo</option>
+                                            <option value="executiva">Suite Executiva</option>
+                                            <option value="presidencial">Suite Presidencial</option>
+                                        </select>
+                                    </div>
+
+                                    <div class="row mb-3">
+                                        <div class="col-md-6 mb-3">
+                                            <label for="adulto" class="form-label">Adulto</label>
+                                            <input type="number" class="form-control" id="adulto" min="1" max="10" required>
+                                        </div>
+                                        <div class="col-md-6 mb-3">
+                                            <label for="crianca" class="form-label">Criança</label>
+                                            <input type="number" class="form-control" id="crianca" min="0" max="10" required>
+                                        </div>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary w-100">Efectuar Pagamento</button>
                                 </div>
                                 <div class="modal-footer">
                                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>

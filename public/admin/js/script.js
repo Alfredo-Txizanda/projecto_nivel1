1c2ac1c8591314dbca073a7c43cdc2289646bae4
Alfredo Txizanda <txizanda.treinamento@gmail.com>
2025-11-29 17:54:40 +0100

    ok

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,10 +1,61 @@
 $(document).ready(function() {
     console.log("Document is ready. script.js loaded.");
 
+    // Função para configurar a validação de datas
+    function setupDateValidation(checkinSelector, checkoutSelector) {
+        const checkinInput = $(checkinSelector);
+        const checkoutInput = $(checkoutSelector);
+
+        if (checkinInput.length && checkoutInput.length) {
+            const today = new Date().toISOString().split('T')[0];
+
+            checkinInput.attr('min', today);
+            
+            // Se o check-in já tiver um valor (no caso de edição), define o min do checkout
+            if(checkinInput.val()){
+                checkoutInput.attr('min', checkinInput.val());
+            } else {
+                checkoutInput.attr('min', today);
+            }
+
+            checkinInput.on('change', function() {
+                if (this.value) {
+                    checkoutInput.attr('min', this.value);
+                    if (checkoutInput.val() && new Date(checkoutInput.val()) < new Date(this.value)) {
+                        checkoutInput.val('');
+                    }
+                }
+            });
+
+            checkoutInput.on('change', function() {
+                if (this.value && checkinInput.val()) {
+                    const checkInDate = new Date(checkinInput.val());
+                    const checkOutDate = new Date(this.value);
+
+                    if (checkOutDate <= checkInDate) {
+                        alert('A data de Check-out deve ser posterior à data de Check-in.');
+                        this.value = '';
+                    }
+                }
+            });
+        }
+    }
+
+    // Evento para o modal de adicionar reserva
+    $('#addReservaModal').on('show.bs.modal', function () {
+        setupDateValidation('#checkin', '#checkout');
+    });
+
+    // Evento para o modal de editar reserva
+    $('#editReservaModal').on('show.bs.modal', function () {
+        setupDateValidation('#editCheckin', '#editCheckout');
+    });
+
     // Get the initial dashboard content
     var initialContent = $('.content').html();
 
-    $('.sidebar-nav a').on('click', function(e) {
+    // Delegação de eventos para os links da sidebar
+    $(document).on('click', '.sidebar-nav a', function(e) {
         e.preventDefault();
         console.log("Sidebar link clicked.");
 

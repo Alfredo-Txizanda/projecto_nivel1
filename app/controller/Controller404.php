87d3c1ff04be56f1354f40bbe2a515426eabceec
Alfredo-Txizanda <txizanda.treinamento@gmail.com>
2025-12-01 10:12:54 +0100

    Controller404-index

--- app/controller/Controller404.php
+++ app/controller/Controller404.php
@@ -11,7 +11,6 @@
             echo "<h1 style='color: #000; text-align: left; font-size: 2.5em; '>Esta página não existe!</h1>";
         }
 
-
         public function index()
         {
             $this->renderLayout();

34ddb2325e1b43ac71d736553c51199a41ba852a
Alfredo-Txizanda <txizanda.treinamento@gmail.com>
2025-12-01 10:44:23 +0100

    ControllerGaleria-Controller

--- app/controller/ControllerGaleria.php
+++ app/controller/ControllerGaleria.php
@@ -8,10 +8,10 @@
     class ControllerGaleria extends Render implements InterfaceView  {
         public function __construct()
         {
-            $this->setTitulo("Sobre Nós");
+            $this->setTitulo("Galeria de Fotos");
             $this->setDescricao("Projecto de defesa final de curso | TCC");
-            $this->setPalavraChave("Sobre Nós, sobre, sobre o projecto, Sobre o Hotel, Alfredo, Txizanda");
-            $this->setPasta("sobre");
+            $this->setPalavraChave("Fotos, galeria, Hotel, Quartos");
+            $this->setPasta("galeria");
             //$this->renderLayout();
         }
 

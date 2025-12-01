6e06b68632fde830ec91072695796423dd17b534
Alfredo-Txizanda <txizanda.treinamento@gmail.com>
2025-12-01 08:56:51 +0100

    Controller404-Erro

--- app/controller/Controller404.php
+++ app/controller/Controller404.php
@@ -2,9 +2,29 @@
 
     namespace app\controller;
 
-    class Controller404 {
+    /*class Controller404 {
         public function __construct ()
         {
             echo "<h1 style='color: #000; text-align: left; font-size: 2.5em; '>Esta página não existe!</h1>";
         }
+    }*/
+
+    use src\classes\Render;
+    use src\interfaces\InterfaceView;
+
+    class Controller404 extends Render implements InterfaceView  {
+        public function __construct()
+        {
+            $this->setTitulo("Página Inexistente");
+            $this->setDescricao("Projecto final de curso | TCC");
+            $this->setPalavraChave("Erro, Erro 404");
+            $this->setPasta("erro");
+            //$this->renderLayout();
+        }
+
+        public function index()
+        {
+            $this->renderLayout();
+        }
+
     }

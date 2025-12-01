a0066db9502036dcc6b8aafe31d833c33cc372e8
Alfredo-Txizanda <txizanda.treinamento@gmail.com>
2025-12-01 08:49:11 +0100

    Layout-Sobre

--- app/view/Layout.php
+++ app/view/Layout.php
@@ -30,7 +30,7 @@
                         <li class="nav-item"> <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#reservaModal">Reserva</a></li>
                         <li class="nav-item"><a class="nav-link" href="<?= DIRPAGE . 'galeria'; ?>">Galeria</a></li>
                         <li class="nav-item"> <a class="nav-link" href="<?= DIRPAGE . 'admin/login'; ?>">Login</a></li>
-                        <li class="nav-item"> <a class="nav-link" href="<?= DIRPAGE . 'sobre'; ?>">Sobre nós</a></li>
+                        <li class="nav-item"> <a class="nav-link" href="<?= DIRPAGE . 'sobre'; ?>">Sobre Nós</a></li>
                     </ul>
                     <form class="d-flex" role="search">
                         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>

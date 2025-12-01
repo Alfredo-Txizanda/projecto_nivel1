d452ca58244ce61560ce13c7ca54353d779407ac
Alfredo-Txizanda <txizanda.treinamento@gmail.com>
2025-12-01 10:39:56 +0100

    Controller404-Ajustar

--- app/controller/Controller404.php
+++ app/controller/Controller404.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace app\controller;
+    namespace app\controller;
 
-class Controller404 {
-    public function index()
-    {
-        echo "<h1 style='color: #000; font-size: 2.5em; padding: 25px;'>Esta página não Existe</h1>";
+    class Controller404 {
+        public function index()
+        {
+            echo "<h1 style='color: #000; font-size: 2.5em; padding: 25px;'>Esta página não Existe!</h1>";
+        }
     }
-}

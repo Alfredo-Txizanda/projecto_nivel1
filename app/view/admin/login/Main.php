804bb9e97808f65c54e5fbbe561debc03ce148e5
Alfredo-Txizanda <txizanda.treinamento@gmail.com>
2025-12-03 16:49:32 +0100

    Main-Login

--- app/view/admin/login/Main.php
+++ app/view/admin/login/Main.php
@@ -1,18 +1,1 @@
-<div class="card shadow-lg login" style="width: 100%; max-width: 400px;">
-    <div class="card-body p-5">
-        <h3 class="card-title text-center mb-4">Login Administrativo</h3>
-        <form action="" method="post">
-            <div class="mb-3">
-                <label for="username" class="form-label">Utilizador</label>
-                <input type="text" class="form-control" id="username" name="username" required>
-            </div>
-            <div class="mb-3">
-                <label for="password" class="form-label">Palavra-passe</label>
-                <input type="password" class="form-control" id="password" name="password" required>
-            </div>
-            <div class="d-grid">
-                <button type="submit" class="btn btn-primary">Entrar</button>
-            </div>
-        </form>
-    </div>
-</div>
+
